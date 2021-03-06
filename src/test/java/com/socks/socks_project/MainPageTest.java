771a8904729510d0c4c92cedf233efb2ca86f22f
Andrew Fisher <andrewfisher093@gmail.com>
2021-03-06 21:42:36 +0200

    Initial commit

---
+++ src/test/java/com/socks/socks_project/MainPageTest.java
@@ -0,0 +1,60 @@
+package com.socks.socks_project;
+
+import static com.codeborne.selenide.Condition.attribute;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byClassName;
+import static com.codeborne.selenide.Selectors.byId;
+import static com.codeborne.selenide.Selectors.byXpath;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.testng.Assert.assertEquals;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class MainPageTest {
+
+    private final MainPage mainPage = new MainPage();
+
+    @BeforeClass
+    public static void setUpAllure() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @BeforeMethod
+    public void setUp() {
+        Configuration.startMaximized = true;
+        open("https://www.jetbrains.com/");
+    }
+
+    @Test
+    public void search() {
+        mainPage.searchButton.click();
+
+        $(byId("header-search")).sendKeys("Selenium");
+        $(byXpath("//button[@type='submit' and text()='Search']")).click();
+
+        $(byClassName("js-search-input")).shouldHave(attribute("value", "Selenium"));
+    }
+
+    @Test
+    public void toolsMenu() {
+        mainPage.toolsMenu.hover();
+
+        $(byClassName("menu-main__popup-wrapper")).shouldBe(visible);
+    }
+
+    @Test
+    public void navigationToAllTools() {
+        mainPage.seeAllToolsButton.click();
+
+        $(byClassName("products-list")).shouldBe(visible);
+
+        assertEquals(Selenide.title(), "All Developer Tools and Products by JetBrains");
+    }
+}
